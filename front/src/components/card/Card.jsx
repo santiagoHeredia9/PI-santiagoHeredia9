98aca49ee271de034250f8512e20973c904eb467
santiagoHeredia <santiagoherediabrandolini@gmail.com>
2023-11-24 11:18:31 -0300

    React-hooks

--- front/src/components/card/Card.jsx
+++ front/src/components/card/Card.jsx
@@ -1,37 +1,104 @@
 /* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
 import style from "./Card.module.scss";
+import { addFav, removeFav } from "../../redux/actions";
+import { connect } from "react-redux";
+import { useState, useEffect } from "react";
 
-export default function Card(props) {
+function Card({
+  id,
+  image,
+  gender,
+  status,
+  origin,
+  onClose,
+  name,
+  species,
+  addFavs,
+  removeFavs,
+  myFavs
+}) {
+
+
+
+  useEffect(() => {
+    myFavs.forEach((fav) => {
+       if (fav.id === id) {
+          setIsFav(true);
+       }
+    });
+ }, [myFavs]);
+
+
+  const [isFav, setIsFav] = useState(false);
+
+  const handleFavorite = () => {
+    if (isFav) {
+      setIsFav(false);
+      removeFavs(id);
+    } else {
+      setIsFav(true);
+      addFavs({id,
+        image,
+        gender,
+        status,
+        origin,
+        onClose,
+        name,
+        species,});
+    }
+  };
+
   return (
     <div className={style.cartas}>
-      <button className={style.boton} onClick={() => props.onClose(props.id)}>
+      {isFav ? (
+        <button onClick={handleFavorite}>❤️</button>
+      ) : (
+        <button onClick={handleFavorite}>🤍</button>
+      )}
+
+      <button className={style.boton} onClick={() => onClose(id)}>
         X
       </button>
-      <Link className={style.link} to={`/detail/${props.id}`}>
-        <h2>{props.name}</h2>
+      <Link className={style.link} to={`/detail/${id}`}>
+        <h2>{name}</h2>
       </Link>
-      <img src={props.image} alt="characterimage" className={style.img} />
+      <img src={image} alt="characterimage" className={style.img} />
       <div className={style.info}>
         <h2>
           <span className={style.span}>Gender: </span>
-          {props.gender}
+          {gender}
         </h2>
         <h2>
           <span className={style.span}>Specie: </span>
-          {props.species}
+          {species}
         </h2>
       </div>
       <div className={style.info2}>
         <h2>
           <span className={style.span}>Status: </span>
-          {props.status}
+          {status}
         </h2>
         <h2>
           <span className={style.span}>From: </span>
-          {props.origin}
+          {origin}
         </h2>
       </div>
     </div>
   );
 }
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addFavs: () => dispatch(addFav),
+    removeFavs: () => dispatch(removeFav),
+  };
+};
+
+const mapStateToProps = (state) => {
+  return {
+    myFavs: state.myFavorites,
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Card);
