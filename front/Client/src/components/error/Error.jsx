a43cc74426df25c13ada4209198bb1862e4845e7
santiagoHeredia <santiagoherediabrandolini@gmail.com>
2023-12-12 16:07:59 -0300

    pi

--- front/Client/src/components/error/Error.jsx
+++
@@ -1,6 +0,0 @@
-const Error= () => {
- return <h1>404 PAGE NOT FOUND</h1>
-
-}
-
-export default Error
--- front/Client/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import reducer from "./reducer";
-
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
-
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)));
-
-export default store;
